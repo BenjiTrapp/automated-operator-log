9919eaa4022cfcbafc61b6a2511bd05e0b9333a7
Benjamin-Yves Trapp <benji.trapp@gmail.com>
2024-03-10 09:30:38 +0000

    Daemonized

--- auditd_json_converter.py
+++ auditd_json_converter.py
@@ -2,14 +2,15 @@ import argparse
 import time
 import sys
 import json
+import glob
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Convert an auditd file or line into JSON')
-    parser.add_argument('-f', '--file', help='Specify the complete path of file to convert to JSON', required=False)
+    parser.add_argument('-f', '--file', help='Specify the complete path of file to convert to JSON', default='/var/log/audit/')
     parser.add_argument('-l', '--line', help='Specify a single line in "" to convert to JSON', required=False)
     parser.add_argument('-o', '--output_file', help='Specify a destination file for file conversion output', required=False)
-    parser.add_argument('--operator-log', help='Specify the type of log to process', choices=['execve', 'execveat'], required=False)
-    return parser.parse_args()
+    parser.add_argument('-ol', '--operator-log', help='Specify the type of log to process', default=['execve', 'execveat'], nargs='*')
+    return parser.parse_args(), parser
 
 def get_time(line):
     timestamp = line.replace('msg=audit(','').replace('):','').split(':')
@@ -26,13 +27,15 @@ def make_readable(key):
         'exe': 'executable'
     }.get(key, key)
 
-def process_file(path, output, operator_log):
+def process_file(path, output, operator_logs):
     entries = []
     with open(path,'r') as f:
         for line in f:
-            if operator_log in line:
-                entry = process_line(line.replace('\n',''))
-                entries.append(entry)
+            for operator_log in operator_logs:
+                if operator_log in line:
+                    entry = process_line(line.replace('\n',''))
+                    entries.append(entry)
+                    break  # Stop checking other operator_logs once a match is found
 
     if output:
         with open(output, 'w') as w:
@@ -54,11 +57,16 @@ def process_line(line):
     return entry
 
 def main():
-    args = parse_args()
+    args, parser = parse_args()
 
     if args.file:
-        print("Converting file...")
-        process_file(args.file, args.output_file, args.operator_log)
+        print("Converting file(s)...")
+        if args.file.endswith('.log'):
+            process_file(args.file, args.output_file, args.operator_log)
+        else:
+            # If a directory is provided, process all .log files in it
+            for log_file in glob.glob(args.file + '*.log'):
+                process_file(log_file, args.output_file, args.operator_log)
         print("Conversion completed!")
     elif args.line:
         print("Converting line...")
